f0b30b1a386977ce0d917f531c18117d7ea8efd5
miao <miaomiao.cai@senscape.com.cn>
2020-09-17 14:56:02 +0800

    1. 51-N_Queen

---
+++ 51-N_Queen/main.c
@@ -0,0 +1,23 @@
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+#include <stdio.h>
+#include <stdlib.h> 
+#include <stdbool.h>
+
+
+
+char *** solveNQueens(int n, int* returnSize, int** returnColumnSizes) {
+    char queen[n][n];
+    printf("%d\n", SIZE);
+    return NULL;
+}
+
+int main(void) {
+    solveNQueens(-1, NULL, NULL);
+    return 0;
+}
+
+// gcc -ggdb main.c 
